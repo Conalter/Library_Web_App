e46fb6ebae9acff961a47d3b21a61d8bd9edf212
Lyle Mitchell <ljwmitchell@gmail.com>
2018-05-23 14:51:53 +0100

    adds other aspects

--- src/main/java/borrowers/Borrower.java
+++ src/main/java/borrowers/Borrower.java
@@ -1,19 +1,55 @@
 package borrowers;
 
+import books.Book;
+import com.sun.javaws.jnl.LibraryDesc;
+
 import java.util.ArrayList;
 
 public class Borrower {
 
     private String Name;
-    private ArrayList<Books> books;
+    private ArrayList<Book> books;
     private String Library;
     private int id;
 
-    public Borrower(String name books, String library) {
+    public Borrower(String name, books, Library library) {
         this.Name = name;
-        this.Books = new ArrayList<Books>();
+        this.Book = new ArrayList<Book>();
         Library = new library;
     }
 
+    public Borrower() {
+    }
 
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public String getLibrary() {
+        return Library;
+    }
+
+    public void setLibrary(String library) {
+        Library = library;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
