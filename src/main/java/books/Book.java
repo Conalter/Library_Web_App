6b1fcabd89861cf40fcc79673699b802541aa926
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-23 14:19:30 +0100

    Book class set up with constructor, getters and setters

--- src/main/java/books/Book.java
+++ src/main/java/books/Book.java
@@ -1,4 +1,60 @@
 package books;
 
 public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private Borrower borrower;
+    private Library library;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, Borrower borrower, Library library) {
+        this.title = title;
+        this.author = author;
+        this.borrower = borrower;
+        this.library = library;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Borrower getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Borrower borrower) {
+        this.borrower = borrower;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
 }
