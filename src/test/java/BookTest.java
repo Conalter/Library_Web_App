7afc372f31438ffe2a9ffd5503e502f9b83273df
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-23 14:15:14 +0100

    intial test set up

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,4 +1,19 @@
+import books.Book;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BookTest {
 
+    Book book1;
+    Book book2;
+    Book book3;
 
-}
+
+    @Before
+    public void setUp() throws Exception {
+     book1 = new Book("Great Expectations", "Charles Dickens");
+     book2 = new Book("Pride and Prejudice", "Jane Austen");
+     book3 = new Book("War and Peace", "Leo Tolstoy");
+    }
+
+    }
